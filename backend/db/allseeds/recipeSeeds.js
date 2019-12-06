aae9b894e79d2cd8f3ee0306b8ed64b988b8d6d0
Michael Adair <mjadair91@gmail.com>
2019-12-06 10:59:23 +0000

    Updated Restaurant Seeds to match Schema

--- backend/db/allseeds/recipeSeeds.js
+++ backend/db/allseeds/recipeSeeds.js
@@ -6,6 +6,7 @@
 const recipeSeed = (user) => {
   return [{
     name: 'Pappardelle with nduja, mascarpone and lemon',
+    author: 'Michael',
     ingredients: ['Tagliatelle', 'Nduja', 'Mascarpone', 'Lemons (zest and juice)', 'Parsley', 'Parmesan', 'Olive oil'],
     method: ['Fry the nduja in the olive oil.', 'When the nduja has melted, add the lemon zest and juice, allow to infuse for a couple of minutes.', 'Add the mascarpone, reduce heat and stir', 'Once the sauce has thickened, add the cooked pasta and a tablespoon of its cooking water.', 'Serve topped with chopped parsley and parmesan'],
     image: 'https://i.imgur.com/rEM6L2U.jpg',
@@ -18,6 +19,7 @@ const recipeSeed = (user) => {
   },
   {
     name: 'Sourdough Bread',
+    author: 'Michael',
     ingredients: ['400g Strong White Bread Flour', '145g Rye Starter', '50g Rye Flour', '50g Wholemeal Flour', '12g Sea Salt', 'Olive Oil', 'Semolina (for dusting)' ],
     method: ['Mix the flours and the starter.', 'Knead until the gluten develops.', 'Fold, then rest for 12 hours. Bake.'],
     image: 'https://imgur.com/PQaorBZ',
@@ -30,13 +32,14 @@ const recipeSeed = (user) => {
   },
   {
     name: 'Ramen Noodles',
+    author: 'Michael',
     ingredients: ['Eggs', 'Noodles', 'Soy Sauce', 'Spring Onions', 'Chicken Stock', 'Protein of your choice - e.g Chicken, Tofu', 'Vegetables of your choice' ],
     method: ['Stick all the ingredients into a pot. Make sure they are cooked.', 'Delicious.'],
     image: 'https://i.imgur.com/1YKD8b1.jpg',
     category: ['Asian'],
     type: ['Lunch', 'Dinner'],
     dietary: ['Vegetarian'],
-    complexity: 'Medium's,
+    complexity: 'Medium',
     time: 'One Hour',
     user: user
   }]
